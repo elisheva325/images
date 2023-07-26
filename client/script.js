c6781858f9051da755d9d012c4f60aa587dd15e1
Miriam Wisky <miriamwisky11@gmail.com>
2023-07-26 16:34:39 +0300

    last version

--- client/script.js
+++ client/script.js
@@ -1,48 +1,50 @@
-//const{ text1}=require('express');
 let btn_image, text_image, image, loader
+let search_txt, search_res, search_btn , currentPrase;
 
-let search_txt, search_res, search_btn
 function init() {
     search_txt = document.getElementById("search_txt")
     search_res = document.getElementById("search_res")
     image = document.getElementById("my_img")
-    // text_image = document.getElementById("text_image")
+    
     new_img=document.getElementById("new_img")
     text_add=document.getElementById("add")
     phrase=document.getElementById("phrase")
     likes=document.getElementById("likes")
-    phrase2=document.getElementById("phrase2")
-    likes2=document.getElementById("likes2")
-    
-    search_btn = document.getElementById("search_btn")
-    search_btn.onclick = click_fn
-
-    btn_image = document.getElementById("btn_img")
-    btn_image.onclick = phrase_with_image;
+    like=document.getElementById("like");
+    btn=document.getElementById("btn")
+    btn.onclick = update_like;
 
     add_btn=document.getElementById("add_btn")
     add_btn.onclick=addPhrase;
     
+    btn.style.display = "none"
+    likes.style.display = "none"
+    image.style.display = "none"
+    random_images();
+    setInterval(random_images,10000);
+}
+async function update_like(){
+    likes.innerText=` ${parseInt(likes.innerHTML) + 1}`;
+    url = `http://localhost:3000/put/${currentPrase}`
+    let li = parseInt(likes.innerHTML)  ;
+    const req = {"updLike" : li};
     
-    
-    loader = document.getElementById("loader")
-    loader.style.display = "none"
-    // setInterval(random_images,10000)
+    res = await axios.put(`${url}` , req);
 }
 async function random_images(){
-
+    btn.style.display = "block"
     image.style.display = "block"
-   
-    // text = text_image.value
-    url = `http://localhost:8080/get`
+    likes.style.display = "block"
+
+    url = `http://localhost:3000/get`
     
     res = await axios.get(url)
-   
-    console.log(res.data);   
+    
     image.style.display = "block";
     image.src = res.data["img"];
-    // phrase.innerText=res.data["prs"]["phrase"];
-    // likes.innerText=`likes: ${res.data["prs"]["likes"]}`;
+    phrase.innerText=res.data["prs"]["phrase"];
+    currentPrase = res.data["prs"]["phrase"];
+    likes.innerText=` ${res.data["prs"]["likes"]}`;
 
 }
 async function addPhrase(){
@@ -51,55 +53,7 @@ async function addPhrase(){
     data={
         "text":text1
     }
-    alert(text1);
-    url = `http://localhost:8080/post`
+    url = `http://localhost:3000/post`
     res=await axios.post(url,data) ; 
-    console.log(res.data);
-
-
-}
-async function phrase_with_image(){
-    loader.style.display = "block"
-    new_img.style.display = "none"
-    // text = text_image.value
-    alert("1");
-    url = `http://localhost:8080/get/image`
-    res = await axios.get(url)
-    alert("2");
-    loader.style.display = "none"
-    new_img.style.display = "block"
-
-    console.log(res.data)
-    new_img.src = res.data["img"];
-    // phrase2.innerText=res.data["prs"]["phrase"];
-    // likes2.innerText=`likes: ${res.data["prs"]["likes"]}`;
-}
-
-async function click_fn() {
-    loader.style.display = "block"
-    image.style.display = "none"
-    text ="home"
-    url = `http://localhost:3000/get`
-    res = await axios.get(url)
-    loader.style.display = "none"
-    image.style.display = "block"
-    console.log(res)
-
-    console.log(res)
-    image.src = res.data 
-}
-
-async function serach_fn(){
-    text = search_txt.value
-
-    url = `http://localhost:8080/getAll/${text}`
-    res = await axios.get(url)
-    console.log(res.data)
-
-    imgs_str = res.data.map(img => `<img src=${img.image_url} width="100px">`)
-    console.log(imgs_str.join(""))
-    search_res.innerHTML = imgs_str.join("")
-
-
 
 }
--- index.js
+++ index.js
@@ -1,158 +1,115 @@
-
-
-// apiKey = "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"
-apiKey="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";
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { Configuration, OpenAIApi } = require("openai");
-// const axios= require('axios');
-// const mongoose = require('mongoose')
 
 const app = express();
-// const { OPENAI_API_KEY } = process.env;
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 1;
-const configuration = new Configuration({
-    apiKey: apiKey,
-});
-const openai = new OpenAIApi(configuration);
 
-let phrases=[
-    {"phrase":"A journey of a thousand miles begins with a single step" ,"likes":0},
-    {"phrase":"In the midst of chaos, there is also opportunity" ,"likes":0},
-    {"phrase":"The best way to predict the future is to create it" ,"likes":0}
-
-
-]
-
-// mongo_url = "mongodb+srv://sarae:1234@serverdb.zth5sjz.mongodb.net/?retryWrites=true&w=majority"
-
-// mongoose.set('strictQuery', false)
-// mongoose.connect(mongo_url)
-
-
-// const imageSchema = new mongoose.Schema({
-//     image_text: String,
-//     image_url: String,
-// })
-
-// const Image = mongoose.model('Image', imageSchema)
 
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
+
+let phrases=[
+    {"phrase":"A journey of a thousand miles begins with a single step" ,"likes":0},
+    {"phrase":"In the midst of chaos, there is also opportunity" ,"likes":0},
+    {"phrase":"The best way to predict the future is to create it" ,"likes":0},
+    {"phrase":"never say never" ,"likes":0},
+    {"phrase":"hello world" ,"likes":0},
+    {"phrase":"The book was on the table" ,"likes":0},
+    {"phrase":"We camped by the brook" ,"likes":0},
+    {"phrase":"He knew it was over the rainbow" ,"likes":0},
+    {"phrase":"She was lost in the dark of night" ,"likes":0},
+    {"phrase":"He was between a rock and a hard place" ,"likes":0},
+    {"phrase":"I waited for a while" ,"likes":0},
+    {"phrase":"She smelled of strawberries and cream" ,"likes":0},
+    {"phrase":"He won the challenge against all odds" ,"likes":0}
+
+]
+let images = ["https://images.unsplash.com/photo-1690148136337-2304c30b5420?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+,"https://images.unsplash.com/photo-1690197104559-231ea4d93298?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=771&q=80"
+,"https://images.unsplash.com/photo-1690177930724-2aef91b1d2f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+,"https://images.unsplash.com/photo-1690177930780-6b3c800a2f57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+,"https://images.unsplash.com/photo-1690148136337-2304c30b5420?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+,"https://images.unsplash.com/photo-1690112018874-08cb5933648f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80"
+,"https://images.unsplash.com/photo-1582198239434-c9752603da1f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1688063969434-e81d6b2d3a26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1603077787992-7efca4cdffa5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=346&q=80"
+,"https://images.unsplash.com/photo-1651062108412-36a68f3748dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+,"https://images.unsplash.com/photo-1601883165851-208729ce4b55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+,"https://images.unsplash.com/photo-1610631210464-d3df3b970de4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80"
+,"https://images.unsplash.com/photo-1612219890950-afddcc6fcf98?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1487460966527-a9c5c7b16625?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=493&q=80"
+,"https://images.unsplash.com/photo-1465479499730-d2c36f03ef87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80"
+,"https://images.unsplash.com/photo-1663481083247-d16f74e6b059?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1542773049-ccc00e2a69d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+,"https://images.unsplash.com/photo-1552624189-cf10026a305b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80"
+,"https://images.unsplash.com/photo-1688464592993-974c88086521?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1688901724018-8fe3a444312d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1688841471400-bd611ad83b2b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1688651139745-606898a43b52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1688744310248-64ac4d0c766e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1688634254595-714b709f50ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=390&q=80"
+,"https://images.unsplash.com/photo-1689749160960-3a6d965d0fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=342&q=80"
+,"https://images.unsplash.com/photo-1689657074131-ae13b6b4e9c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1688143695340-4d4c4c491bd9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=441&q=80"
+,"https://images.unsplash.com/photo-1689427147115-ee9ea359b658?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+,"https://images.unsplash.com/photo-1678542806833-f4e6be5c9f29?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+,"https://plus.unsplash.com/premium_photo-1661591436759-3a471d5d1b5a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1682336869523-2c6859f781cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+,"https://plus.unsplash.com/premium_photo-1678051339094-8fdc47a91e51?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://plus.unsplash.com/premium_photo-1680087006074-b615c3108bf5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80"
+,"https://images.unsplash.com/photo-1613591797540-862e54975da8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80"
+,"https://plus.unsplash.com/premium_photo-1686904394657-f7ea270c8161?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80"
+,"https://plus.unsplash.com/premium_photo-1686904395497-ef4e38fc551b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://plus.unsplash.com/premium_photo-1670270204756-d53bd70589a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://plus.unsplash.com/premium_photo-1683892045230-49e7b7a0912e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+,"https://images.unsplash.com/photo-1689198163336-c18bbdec27dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+,"https://images.unsplash.com/photo-1688877275094-d56606d9ca02?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1688587737809-d9dabcad1bdc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+,"https://images.unsplash.com/photo-1686749534352-cd3c37b744d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+,"https://images.unsplash.com/photo-1687027054090-f65b0e5cb2d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=385&q=80"
+,"https://images.unsplash.com/photo-1690138500311-2edf767bbbbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+]
+
 app.get('/get',async(req,res)=>{
     try{
-        const response = await openai.createImage({
-            prompt: 'random',
-            n: 1,
-            size: "512x512",
-        });
         const res1={
-            "img":response.data.data[0].url,
+            "img": images[Math.floor(Math.random()*images.length)],
             "prs":phrases[Math.floor(Math.random()*phrases.length)]
         }
-        console.log(res1);
-        res.send("hii")
-
+        res.send(res1);
+       
 
     }catch(err){
-       res.send(err.message+" error");
+       res.send(err.message);
     }
 })
 
-// app.get("/get/:image", async (req, res) => {
-//     let prompt = req.params.image
-//     console.log(prompt)
-//     try {
-//         const response = await openai.createImage({
-//             prompt,
-//             n: 1,
-//             size: "512x512",
-//         });
-//         console.log("djks;ls");
-       
-//         console.log(response.data.data[0].url);
-//         res.send(response.data.data[0].url);
-//     } catch (err) {
-//         res.send(err.message+" error");
-//     }
 
-// });
 
-// app.get('/get', async (req, res) => {
-//     console.log(req.body+"req");
-//      prompt="random";
-  
-//     try {
-//         const response = await openai.createImage({
-//             prompt,
-//             n: 1,
-//             size: "512x512",
-//         });
-//         console.log("ks");
-        
-//         const res1={
-//             "img":response.data.data[0].url,
-//             "prs":phrases[Math.floor(Math.random()*phrases.length)]
-//         }
-//         // let res1="lkdlk"
-//         console.log(res1+"res1");
-        
-//         res.send(res1);
-
-//     } catch (err) {
-//         console.log("erorrr")
-//         res.send(err.message);
-//     }
-
-// });
-// app.get("/get/image", async (req, res) => {
-//     console.log(req.body+"req");
-//     prompt="random";
-//     try {
-//         const response = await openai.createImage({
-//             prompt,
-//             n: 1,
-//             size: "512x512",
-//         });
-//         console.log("ks");
-        
-//         const res1={
-//             "img":response.data.data[0].url,
-//             "prs":phrases[Math.floor(Math.random()*phrases.length)]
-//         }
-//         console.log(res1+"res1");
-        
-//         res.send(res1);
-
-//     } catch (err) {
-//         res.send(err.message);
-//     }
-
-// });
 app.post('/post', async(req,res)=>{
     let phrase={"phrase":req.body["text"],"likes":0};
     phrases.push(phrase);
 });
 
 
-app.get("/getAll/:image", async (req, res) => {
-    let prompt = req.params.image
-    console.log(prompt)
+
+app.put("/put/:prs", async (req, res) => {
+    const resourceId = req.params.prs;
+    const newData = req.body["updLike"];
     try {
-        
-        result = await Image.find({image_text:prompt})
-        res.send(result);
+        const foundObject = phrases.find(obj => obj.phrase === resourceId);
+        if (foundObject) {
+            foundObject.likes = newData;
+        }
+        res.send("updated successfully");
     } catch (err) {
         res.send(err.message);
     }
 
 });
-
-
-
 
 
 app.listen(3000, () => {
